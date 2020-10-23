f02765dc3d69e855b277305c77b244e7bff7dc14
trodrsil <trodrsil@SAO-8B3SHJ2.usersad.everis.int>
2020-10-22 23:23:37 -0300

    Segundo Commit ao GitHub

--- ProjetoTestandoGithub/src/ClassTestGithub.java
+++ ProjetoTestandoGithub/src/ClassTestGithub.java
@@ -4,6 +4,7 @@ public class ClassTestGithub {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("\nCommitando no GitHub");
+		System.out.println("\nSegundo commit no GitHub");
 	}
 
 }
